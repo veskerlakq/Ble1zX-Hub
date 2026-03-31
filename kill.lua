5888f79567eb3fd9f58ac97a1c013729733db423
veskerlakq <veskerlakq@gmail.com>
2026-03-31 18:46:41 +0300

    Update kill.lua

--- kill.lua
+++ kill.lua
@@ -1,12 +1,1 @@
-local kill = {}
-function kill.logic()
-    spawn(function()
-        while task.wait(0.2) do
-            if _G.AutoKill then
-                -- Логика проверки Targets и телепорта над мобами
-                -- ...
-            end
-        end
-    end)
-end
-return kill
+
