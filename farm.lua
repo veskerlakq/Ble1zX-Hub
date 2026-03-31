a3f397ee024a39ad283e63b32a5ff98f7cfe6bbe
veskerlakq <veskerlakq@gmail.com>
2026-03-31 18:12:21 +0300

    Update farm.lua

--- farm.lua
+++ farm.lua
@@ -1,1 +1,14 @@
-
+local farm = {}
+function farm.start()
+    spawn(function()
+        while true do
+            task.wait(0.5)
+            if _G.Farm then
+                local target = _G.Fields[_G.SelectedField]
+                -- Полет и копка (код из прошлых версий)
+                -- ... (логика TweenService и CollectPollen)
+            end
+        end
+    end)
+end
+return farm
